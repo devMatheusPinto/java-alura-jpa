0618dca254333cc83d4110a1b0dea64dd219501a
devMatheusPinto <matheuspinto@outlook.com>
2023-10-03 18:03:08 -0300

    Aula 3

---
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -0,0 +1,32 @@
+package br.com.alura.loja.testes;
+
+import br.com.alura.loja.dao.CategoriaDao;
+import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Categoria;
+import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+
+public class CadastroDeProduto {
+    public static void main(String[] args) {
+        Categoria celulares = new Categoria("CELULARES");
+
+        Produto celular = new Produto("Redmi 1", "Muito legal", new BigDecimal("800"), celulares);
+
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+        CategoriaDao categoriaDao = new CategoriaDao(em);
+
+        em.getTransaction().begin();
+
+        produtoDao.cadastrar(celular);
+        categoriaDao.cadastrar(celulares);
+
+        em.getTransaction().commit();
+        em.close();
+    }
+}
