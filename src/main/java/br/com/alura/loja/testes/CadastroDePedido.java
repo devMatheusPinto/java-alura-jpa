3e330c6e4bbdd880077c32988c9b79aaad56070c
devMatheusPinto <matheuspinto@outlook.com>
2023-10-05 11:29:41 -0300

    Aula 1

--- src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
@@ -1,6 +1,7 @@
 package br.com.alura.loja.testes;
 
 import br.com.alura.loja.dao.CategoriaDao;
+import br.com.alura.loja.dao.PedidoDao;
 import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.*;
 import br.com.alura.loja.util.JPAUtil;
@@ -11,13 +12,16 @@ import java.math.BigDecimal;
 public class CadastroDePedido {
     public static void main(String[] args) {
         cadastrarProduto();
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+        Produto produto = produtoDao.buscarPorId(1l);
 
         Cliente cliente = new Cliente("Matheus", "123456");
         Pedido pedido = new Pedido(cliente);
-        pedido.adicionarItem(new ItemPedido());
+        pedido.adicionarItem(new ItemPedido(10, pedido, produto));
 
-        EntityManager em = JPAUtil.getEntityManager();
-        ProdutoDao produtoDao = new ProdutoDao(em);
+        PedidoDao pedidoDao = new PedidoDao(em);
+        pedidoDao.cadastrar(pedido);
     }
 
     private static void cadastrarProduto() {
