6f0a724936cf7f9342d2470439b4f541e2eafb4e
devMatheusPinto <matheuspinto@outlook.com>
2023-10-05 11:41:33 -0300

    Aula 1

--- src/main/java/br/com/alura/loja/modelo/Pedido.java
+++ src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -20,7 +20,7 @@ public class Pedido {
     @ManyToOne
     private Cliente cliente;
 
-    @OneToMany(mappedBy = "pedido")
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
     private List<ItemPedido> itens = new ArrayList<>();
 
     public LocalDate getDataCadastro() {
